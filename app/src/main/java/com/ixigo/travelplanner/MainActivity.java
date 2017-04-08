82c807a0a6c24adf99e58faf6ff66428a41dae21
Salil.Kaul <salilk@intelligrape.com>
2017-04-08 12:48:55 +0530

    test

--- app/src/main/java/com/ixigo/travelplanner/MainActivity.java
+++ app/src/main/java/com/ixigo/travelplanner/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //salil first commit
-
+        //Commit on network
         setContentView(R.layout.activity_main);
     }
 }
